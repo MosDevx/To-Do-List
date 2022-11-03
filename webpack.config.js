d5a4fe378295820d25cd18aed02bb60a1e0e4059
MosDevx <mosesk.wamae@gmail.com>
2022-11-03 22:05:10 +0300

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ const path = require('path');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 const stylesHandler = isProduction
