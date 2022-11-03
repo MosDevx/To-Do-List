a9455bc0c1854801ab106e1c6bd85360d290b853
MosDevx <mosesk.wamae@gmail.com>
2022-11-03 21:39:54 +0300

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -8,20 +8,20 @@ const todoSorter = (a, b) => (a.index - b.index);
 
 const createTodoItem = ({ index, isCompleted, task }) => {
   const parentList = document.createElement('li');
-	parentList.classList.add("todo-item")
+  parentList.classList.add('todo-item');
   parentList.dataset.id = index;
 
   const completedCheckbox = document.createElement('INPUT');
   completedCheckbox.setAttribute('type', 'checkbox');
   completedCheckbox.setAttribute('checked', isCompleted);
-	completedCheckbox.classList.add('todo-checkbox')
+  completedCheckbox.classList.add('todo-checkbox');
   parentList.appendChild(completedCheckbox);
 
   const todoInput = document.createElement('INPUT');
   todoInput.setAttribute('type', 'input');
   todoInput.setAttribute('readonly', true);
   todoInput.setAttribute('value', task);
-	todoInput.classList.add("todo-input")
+  todoInput.classList.add('todo-input');
   parentList.appendChild(todoInput);
 
   const menuIcon = document.createElement('i');
