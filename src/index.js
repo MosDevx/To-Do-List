81d7763400af058e0abbe8e2772ddcd1b253f2b4
MosDevx <mosesk.wamae@gmail.com>
2022-11-05 08:11:28 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,89 +1,9 @@
 import './style.css';
+import loadTodos from './todoCrud.js';
+// import Todo from './Todo.js';
 
-import Todo from './Todo.js';
+// populateTodoList(Todo.getAllTodos(), todoContainer);
 
-const todoContainer = document.getElementById('todo-container');
-const newTodoInput = document.getElementById('new-todo-input');
-const todoSorter = (a, b) => (a.index - b.index);
-
-const createTodoItem = (todo) => {
-  const parentList = document.createElement('li');
-  parentList.classList.add('todo-item');
-  parentList.dataset.id = todo.index;
-
-  const completedCheckbox = document.createElement('INPUT');
-  completedCheckbox.setAttribute('type', 'checkbox');
-  completedCheckbox.checked = todo.isCompleted;
-  completedCheckbox.classList.add('todo-checkbox');
-  parentList.appendChild(completedCheckbox);
-
-  const todoInput = document.createElement('INPUT');
-  todoInput.setAttribute('type', 'input');
-  todoInput.setAttribute('readonly', true);
-  todoInput.setAttribute('value', todo.task);
-  todoInput.classList.add('todo-input');
-
-  todoInput.addEventListener('click', () => {
-    todoInput.removeAttribute('readonly');
-  });
-
-  todoInput.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      const newTask = todoInput.value;
-      todo.updateTodo(newTask);
-      todoInput.setAttribute('readonly', true);
-      todoInput.blur();
-    }
-  });
-
-  todoInput.addEventListener('blur', () => {
-    const newTask = todoInput.value;
-    todo.updateTodo(newTask);
-    todoInput.setAttribute('readonly', true);
-  });
-  parentList.appendChild(todoInput);
-
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('button');
-
-  // const menuIcon = document.createElement('i');
-  const deleteIcon = document.createElement('i');
-  deleteIcon.classList.add('fa-solid', 'fa-trash');
-  // menuIcon.classList.add('fa-solid', 'fa-ellipsis-vertical');
-
-  deleteButton.appendChild(deleteIcon);
-
-  deleteButton.addEventListener('click', () => {
-    todo.deleteTodo();
-    const mainContainer = parentList.parentNode;
-    mainContainer.removeChild(parentList);
-  });
-  parentList.appendChild(deleteButton);
-
-  return parentList;
-};
-
-const populateTodoList = (todoArray, todoContainer) => {
-  todoArray.sort(todoSorter);
-  todoArray.forEach((todo) => {
-    const todoItem = createTodoItem(todo);
-    todoContainer.appendChild(todoItem);
-  });
-};
-
-// add new todo element
-
-newTodoInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter') {
-    e.preventDefault();
-    const task = newTodoInput.value;
-    const newTodo = new Todo(task);
-    const newTodoItem = createTodoItem(newTodo);
-    todoContainer.appendChild(newTodoItem);
-    newTodoInput.value = '';
-    newTodoInput.blur();
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  loadTodos();
 });
-
-populateTodoList(Todo.getAllTodos(), todoContainer);
