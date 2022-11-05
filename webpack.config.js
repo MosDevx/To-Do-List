af9cd7810c7e7b7c8ecfd4346df15e988523f158
MosDevx <mosesk.wamae@gmail.com>
2022-11-05 07:52:54 +0300

    Revert "Basic structure"

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const stylesHandler = 'style-loader';
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : 'style-loader';
 
 const config = {
   entry: './src/index.js',
@@ -18,7 +21,7 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: './src/index.html',
     }),
 
     // Add your plugins here
@@ -48,6 +51,8 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = 'production';
+
+    config.plugins.push(new MiniCssExtractPlugin());
   } else {
     config.mode = 'development';
   }
